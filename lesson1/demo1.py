7a6f01b61ef0eea04371b2d9ef9cc4f8acffbbe1
liaozhicheng.cn@163.com <liaozhicheng.cn@163.com>
2016-07-24 14:59:32 +0800

    add some comment

--- lesson1/demo1.py
+++ lesson1/demo1.py
@@ -14,5 +14,8 @@ def sigmod(x):
     return y
 
 
+# __name__ == '__main__' 表示只有当该文件作为主模块运行时，if 语句后面的代码才会运行
+# 而如果该文件是通过 import 引入到其他模块中使用时，if 后面的语句并不会执行
+# 通常可用来将一些内部测试代码放在 if 语句后面
 if __name__ == '__main__':
     print sigmod(1)
